60b2357b8df524f16601b6a1effedf3080a55616
rluisr <detteiu.x14th@gmail.com>
2020-04-24 01:40:42 +0900

    Change multipart to query parameter

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -10,8 +10,7 @@ import (
 
 func EncodeHandler(storage storages.IStorage) http.Handler {
 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		if url := r.PostFormValue("url"); url != "" {
-			log.Print("save url: " + url)
+		if url := r.URL.Query().Get("url"); url != "" {
 			_, err := w.Write([]byte(storage.Save(url)))
 			if err != nil {
 				log.Printf("err: %v\n", err)
@@ -26,7 +25,6 @@ func EncodeHandler(storage storages.IStorage) http.Handler {
 func DecodeHandler(storage storages.IStorage) http.Handler {
 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
 		code := r.URL.Path[len("/dec/"):]
-		log.Print("read saved url for code: " + code)
 
 		url, err := storage.Load(code)
 		if err != nil {
@@ -52,7 +50,6 @@ func DecodeHandler(storage storages.IStorage) http.Handler {
 func RedirectHandler(storage storages.IStorage) http.Handler {
 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
 		code := r.URL.Path[len("/red/"):]
-		log.Print("redirect to saved url for code: " + code)
 
 		url, err := storage.Load(code)
 		if err != nil {
@@ -65,7 +62,7 @@ func RedirectHandler(storage storages.IStorage) http.Handler {
 			return
 		}
 
-		http.Redirect(w, r, string(url), 301)
+		http.Redirect(w, r, url, 301)
 	}
 
 	return http.HandlerFunc(handleFunc)
